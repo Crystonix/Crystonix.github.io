1330ddba51778d1edc5feedc8aaec2e4ed3a0f0c
Joshua Jung || jj050 <128308156+Crystonix@users.noreply.github.com>
2026-06-23 12:23:06 +0200

    Add centralized legal site data

---
+++ src/data/legal.ts
@@ -0,0 +1,13 @@
+export const legalProfile = {
+  siteName: 'Crystonix Portfolio',
+  ownerName: 'Joshua Jung',
+  address: ['Propst-Laumann-Straße 8', '48727 Billerbeck', 'Deutschland'],
+  email: 'joshua.jung@kjung.net',
+  lastUpdated: '23. Juni 2026',
+  hostingProvider: 'GitHub Pages',
+  hostingProviderName: 'GitHub, Inc.',
+  hostingPrivacyUrl:
+    'https://docs.github.com/en/site-policy/privacy-policies/github-general-privacy-statement',
+  githubUrl: 'https://github.com/Crystonix',
+  siteUrl: 'https://crystonix.github.io',
+} as const;
